279fcd8748fa0e338798919e7ec2567546d42f57
Tkachenko Nikita <tkachenkond00@gmail.com>
2022-01-25 18:21:06 +0200

    created movie details model

---
+++ HW_8/app/src/main/java/com/gh/hw_8/domain/models/MovieDetails.kt
@@ -0,0 +1,40 @@
+package com.gh.hw_8.domain.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@Entity(tableName = "movie_details")
+@JsonClass(generateAdapter = true)
+data class MovieDetails(
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    @Json(name = "id")
+    val movieId: String,
+
+    @ColumnInfo(name = "poster_path")
+    @Json(name = "poster_path")
+    val movieImage: String?,
+
+    @ColumnInfo(name = "original_title")
+    @Json(name = "original_title")
+    val movieName: String,
+
+    @ColumnInfo(name = "release_date")
+    @Json(name = "release_date")
+    val movieReleaseDate: String,
+
+    @ColumnInfo(name = "overview")
+    @Json(name = "overview")
+    val overview: String,
+
+    @ColumnInfo(name = "backdrop_path")
+    @Json(name = "backdrop_path")
+    val backdropPath: String?,
+
+    @ColumnInfo(name = "vote_average")
+    @Json(name = "vote_average")
+    val rating: String,
+)

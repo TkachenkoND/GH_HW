9ac84b81ac309cdff5f854a283c53b01934e4bee
Tkachenko Nikita <tkachenkond00@gmail.com>
2022-01-25 22:21:51 +0200

    added implementation to class MovieListActivityVm

--- HW_8/app/src/main/java/com/gh/hw_8/presentation/viewModel/MovieListActivityViewModel.kt
+++ HW_8/app/src/main/java/com/gh/hw_8/presentation/viewModel/MovieListActivityViewModel.kt
@@ -1,7 +1,20 @@
 package com.gh.hw_8.presentation.viewModel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gh.hw_8.domain.models.MovieDetails
+import com.gh.hw_8.domain.usecase.LoadMovieDetailsUseCase
+import kotlinx.coroutines.launch
 
-class MovieListActivityViewModel(): ViewModel() {
+class MovieListActivityViewModel(private val loadMovieDetailsUseCase: LoadMovieDetailsUseCase): ViewModel() {
+
+    lateinit var moviesListLiveData: LiveData<List<MovieDetails>>
+
+    fun loadMovieList() {
+        viewModelScope.launch {
+                moviesListLiveData = loadMovieDetailsUseCase.execute()
+        }
+    }
 
 }

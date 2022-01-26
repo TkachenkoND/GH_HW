afd056cc70866cd9520c752692b363313390bde7
Tkachenko Nikita <tkachenkond00@gmail.com>
2022-01-26 14:28:16 +0200

    fixed the error in MovieListActivityVm

--- HW_8/app/src/main/java/com/gh/hw_8/presentation/viewModel/MovieListActivityViewModel.kt
+++ HW_8/app/src/main/java/com/gh/hw_8/presentation/viewModel/MovieListActivityViewModel.kt
@@ -1,6 +1,7 @@
 package com.gh.hw_8.presentation.viewModel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.gh.hw_8.domain.models.MovieDetails
@@ -10,11 +11,12 @@ import kotlinx.coroutines.launch
 class MovieListActivityViewModel(private val loadMovieDetailsUseCase: LoadMovieDetailsUseCase) :
     ViewModel() {
 
-    lateinit var moviesListLiveData: LiveData<List<MovieDetails>>
+     private val _moviesListLiveData = MutableLiveData<List<MovieDetails>>()
+     var moviesListLiveData: LiveData<List<MovieDetails>> = _moviesListLiveData
 
     fun loadMovieList() {
         viewModelScope.launch {
-            moviesListLiveData = loadMovieDetailsUseCase.execute()
+            _moviesListLiveData.value = loadMovieDetailsUseCase.execute().value
         }
     }
 }
